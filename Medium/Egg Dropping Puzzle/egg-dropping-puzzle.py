5db3e2248c4a7ddd2f5b0af8cd94b72865f60bdc
Rea Jis <139119147+reajis@users.noreply.github.com>
2023-07-11 22:45:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Egg Dropping Puzzle/egg-dropping-puzzle.py
@@ -0,0 +1,49 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find minimum number of attempts needed in 
+    #order to find the critical floor.
+    def eggDrop(self,eggs,floors):
+       
+        sol =[[0 for i in range(floors +1)]for j in range(eggs)] 
+        for x in range(floors +1):
+            sol[0][x]=x-1
+
+        sol[0][0]=0
+
+        for egg in range(1,eggs):
+            for floor in range(floors+1):
+                if floor == 0 or floor == 1:
+                    sol[egg][floor]=0
+                elif egg > floor:
+
+                    sol[egg][floor] = sol[egg-1][floor]
+                else :
+                    mini = float('inf')
+                    for tried_floor in range(1,floor+1):
+                        maxi =1+max(sol[egg][floor-tried_floor],sol[egg-1][tried_floor-1])
+                        mini = min(mini,maxi)
+                    sol[egg][floor]= mini
+        #print(sol)            
+        return(sol[eggs-1][floors]+1)
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n,k = map(int,input().strip().split())
+        ob=Solution()
+        print(ob.eggDrop(n,k))
+# } Driver Code Ends
