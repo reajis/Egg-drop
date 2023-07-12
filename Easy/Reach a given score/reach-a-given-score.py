1043a518decb3b8d0d8601ee7c76ff5f8790ef2d
Rea Jis <139119147+reajis@users.noreply.github.com>
2023-07-12 18:37:37 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reach a given score/reach-a-given-score.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+def count(n):
+    points =[3,5,10]
+    arr = [[0 for cols in range(n+1)] for rows in range (3)]
+    for j in range (n+1):
+        if  j % points [0] ==0:
+            arr[0][j] = 1 
+    for i in range (1,3):
+        for j in range(n+1):
+            if j < points[i]:
+                arr[i][j] = arr[i-1][j]
+            else:
+                if j == points[i] :
+                    arr[i][j] = arr[i-1][j] +1
+                else :
+                    arr[i][j] = arr[i-1][j] +arr[i][j-points[i]]
+    return(arr[2][n])
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        n = int(input())
+        print(count(n))
+        
+# } Driver Code Ends
